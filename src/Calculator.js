becf035670828e86e0619e2fb56c9f0a0cb6839d
arunraj.r <arunraj.r@nergyindia.com>
2025-01-20 11:11:54 +0530

    first commit

---
+++ src/Calculator.js
@@ -0,0 +1,191 @@
+import React from "react";
+import { addStyles, EditableMathField } from "react-mathquill";
+import { useCalculator } from "./useCalculator";
+
+addStyles();
+
+function Calculator() {
+  const {
+    definedVariables,
+    digitKeys,
+    operationsKeys,
+    expression,
+    result,
+    handleClick,
+    handleChange,
+    clearScreen,
+    backspace,
+    calculate,
+    handleKeyPress,
+    mathQuillConfig,
+    removeVariable,
+    customVariables,
+  } = useCalculator();
+
+  return (
+    <div className="App master-column">
+      <div className="calc-body special-calculator">
+        <h1>Calculator</h1>
+
+        <label className="input-section">
+          <EditableMathField
+            config={mathQuillConfig}
+            latex={expression}
+            onChange={handleChange}
+            onKeyDown={handleKeyPress}
+          />
+          <div className="output">{result ? `= ${result}` : 0}</div>
+        </label>
+
+        {customVariables?.length > 0 && (
+          <div className="variables">
+            <h4>Custom Variables</h4>
+            <ul>
+              {customVariables?.map((ele) => (
+                <li key={`custom-vars-${ele?.label}-${ele?.value}`}>
+                  <div
+                    className="variable-value"
+                    onClick={() => handleClick(ele?.label)} // value
+                  >
+                    {ele?.label} = {ele?.value}
+                  </div>
+                  <div
+                    className="variable-remove"
+                    onClick={() => removeVariable(ele?.label)}
+                  >
+                    ✕
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <div className="button-section">
+          <div className="left-section">
+            {/* NUMBERS */}
+            <div className="digits">
+              {digitKeys?.map(({ label, value }) => (
+                <div
+                  key={`number-keys-${label}-${value}`}
+                  onClick={() => handleClick(label)}
+                  className="btn"
+                >
+                  {label}
+                </div>
+              ))}
+            </div>
+
+            <div className="defined-variables">
+              {definedVariables?.map(({ label, value }) => (
+                <div
+                  key={`defined-vars-${label}-${value}`}
+                  onClick={() => handleClick(label)} // value
+                  className="btn"
+                >
+                  {label}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="right-section">
+            <div className="operations special">
+              <div className="btn" onClick={backspace}>
+                <svg viewBox="0 0 24 24" height="18" width="18">
+                  <path
+                    fill="#000"
+                    d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-3 12.59L17.59 17 14 13.41 10.41 17 9 15.59 12.59 12 9 8.41 10.41 7 14 10.59 17.59 7 19 8.41 15.41 12 19 15.59z"
+                  ></path>
+                </svg>
+              </div>
+              <div className="btn red-button" onClick={clearScreen}>
+                AC
+              </div>
+              <div className="btn" onClick={() => handleClick("%")}>
+                %
+              </div>
+              <div className="btn" onClick={() => handleClick("sin(x)")}>
+                sin
+              </div>
+              <div className="btn" onClick={() => handleClick("cos(x)")}>
+                cos
+              </div>
+              <div className="btn" onClick={() => handleClick("tan(x)")}>
+                tan
+              </div>
+              <div className="btn" onClick={() => handleClick("{x}^{2}")}>
+                <math xmlns="http://www.w3.org/1998/Math/MathML">
+                  <semantics>
+                    <msup>
+                      <mi>x</mi>
+                      <mn>2</mn>
+                    </msup>
+                  </semantics>
+                </math>
+              </div>
+              <div className="btn" onClick={() => handleClick("{x}^{n}")}>
+                <math xmlns="http://www.w3.org/1998/Math/MathML">
+                  <semantics>
+                    <msup>
+                      <mi>x</mi>
+                      <mn>n</mn>
+                    </msup>
+                  </semantics>
+                </math>
+              </div>
+              <div className="btn" onClick={() => handleClick("\\sqrt{n}")}>
+                <math xmlns="http://www.w3.org/1998/Math/MathML">
+                  <semantics>
+                    <msqrt>
+                      <mi>a</mi>
+                    </msqrt>
+                  </semantics>
+                </math>
+              </div>
+              <div className="btn" onClick={() => handleClick("\\sqrt[x]{n}")}>
+                <math xmlns="http://www.w3.org/1998/Math/MathML">
+                  <semantics>
+                    <mroot>
+                      <mi>a</mi>
+                      <mi>n</mi>
+                    </mroot>
+                  </semantics>
+                </math>
+              </div>
+              <div className="btn" onClick={() => handleClick("\\frac{x}{y}")}>
+                <math xmlns="http://www.w3.org/1998/Math/MathML">
+                  <semantics>
+                    <mfrac>
+                      <msup style={{ fontSize: "18px" }}>
+                        <mi>x</mi>
+                      </msup>
+                      <msup style={{ fontSize: "18px" }}>
+                        <mi>y</mi>
+                      </msup>
+                    </mfrac>
+                  </semantics>
+                </math>
+              </div>
+            </div>
+            <div className="operations">
+              {operationsKeys?.map(({ label, value }) => (
+                <div
+                  key={`number-keys-${label}-${value}`}
+                  onClick={() => handleClick(value)}
+                  className="btn"
+                >
+                  {label}
+                </div>
+              ))}
+              <div className="btn red-button" onClick={calculate}>
+                =
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
---
+++ src/useCalculator.js
@@ -0,0 +1,285 @@
+import * as math from "mathjs";
+import { useEffect, useState } from "react";
+import { useImmer } from "use-immer";
+
+export const useCalculator = () => {
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState("");
+  const [customVariables, setCustomVariables] = useImmer([]);
+  const [definedVariables, setDefinedVariables] = useImmer([
+    { label: "Kₖ", value: 10 },
+    { label: "D₁", value: 20 },
+    { label: "Tₓ", value: 20 },
+    { label: "x₇", value: 20 },
+    { label: "H₈", value: 20 },
+    { label: "L₉", value: 20 },
+    { label: "E", value: 30 },
+    { label: "r", value: 40 },
+    { label: "P₁", value: 50 },
+    { label: "P₀", value: 60 },
+    { label: "i", value: 10 },
+    { label: "V₁", value: 70 },
+    { label: "n", value: 20 },
+    { label: "An", value: 20 },
+    { label: "b", value: 30 },
+    { label: "br", value: 60 },
+    {
+      label: "DΔ",
+      value: 100,
+    },
+    {
+      label: "PΔ",
+      value: 40,
+    },
+    {
+      label: "ΔH",
+      value: 80,
+    },
+  ]);
+
+  const digitKeys = [
+    { label: "1", value: "1" },
+    { label: "2", value: "2" },
+    { label: "3", value: "3" },
+    { label: "4", value: "4" },
+    { label: "5", value: "5" },
+    { label: "6", value: "6" },
+    { label: "7", value: "7" },
+    { label: "8", value: "8" },
+    { label: "9", value: "9" },
+    { label: "0", value: "0" },
+  ];
+
+  const operationsKeys = [
+    { label: "÷", value: "\\div" },
+    { label: "×", value: "\\times" },
+    { label: "-", value: "-" },
+    { label: "+", value: "+" },
+    { label: ".", value: "." },
+  ];
+
+  const mathQuillConfig = {
+    restrictMismatchedBrackets: true,
+  };
+
+  const findExistingVar = (keyname) => {
+    return definedVariables?.find((key) => key?.label === keyname);
+  };
+
+  const handleVarValueChange = (key, value) => {
+    setDefinedVariables((draft) => {
+      const existingValue = draft?.find((ele) => ele?.label === key);
+      if (existingValue) {
+        // Object.assign(existingValue, {
+        //   value,
+        // });
+
+        existingValue.value = value;
+      }
+    });
+  };
+
+  const removeVariable = (label) => {
+    setCustomVariables((draft) => {
+      const filtered = draft?.filter((ele) => ele?.label !== label);
+      return filtered;
+    });
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      const input = expression.trim();
+      const match = input.match(
+        /^([a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*(-?\d*\.?\d+)$/
+      );
+      if (match) {
+        const [_, variableName, value] = match;
+
+        //Check is existing var name
+        const existingItem = findExistingVar(variableName);
+
+        if (!!existingItem) {
+          if (parseInt(existingItem[1]) === parseInt(value)) {
+            const text = `No change in value.`;
+            if (window.confirm(text) == true) {
+              return clearScreen();
+            }
+          }
+
+          const text = `This action will replace your defined variable "${variableName}" value from ${existingItem[1]} to ${value}.`;
+          if (window.confirm(text) == true) {
+            return handleVarValueChange(variableName, value);
+          } else {
+            return;
+          }
+        }
+
+        setCustomVariables((draft) => {
+          return [
+            ...draft,
+            {
+              label: variableName,
+              value,
+            },
+          ];
+        });
+        clearScreen();
+      } else {
+        calculate();
+      }
+    }
+  };
+
+  const latexToMathjs = (latex) => {
+    let mathjs = latex;
+
+    // SPECIAL CHARACTERS
+    const regex = /[a-zA-Z][\u2080-\u2089\u2090-\u209C]/g;
+    const matches = mathjs.match(regex);
+
+    if (matches?.length > 0) {
+      matches.forEach((char) => {
+        const character = definedVariables.find((ele) => ele?.label === char);
+        mathjs = mathjs.replace(char, character?.value);
+      });
+    }
+    // SPECIAL CHARACTERS END
+
+    // Fractions
+    mathjs = mathjs.replace(/\\frac{([^{}]+)}{([^{}]+)}/g, "($1)/($2)");
+
+    // Roots
+    mathjs = mathjs.replace(/\\sqrt{([^{}]+)}/g, "sqrt($1)");
+    mathjs = mathjs.replace(/\\sqrt\[([^{}]+)\]{([^{}]+)}/g, "nthRoot($2, $1)");
+
+    // Trigonometric functions
+    mathjs = mathjs.replace(/\\sin\(([^)]+)\)/g, "sin($1)");
+    mathjs = mathjs.replace(/\\cos\(([^)]+)\)/g, "cos($1)");
+    mathjs = mathjs.replace(/\\tan\(([^)]+)\)/g, "tan($1)");
+
+    // Logarithms
+    mathjs = mathjs.replace(/\\log{([^{}]+)}/g, "log10($1)");
+    mathjs = mathjs.replace(/\\ln{([^{}]+)}/g, "log($1)");
+
+    // Constants
+    mathjs = mathjs.replace(/\\pi/g, "PI");
+    mathjs = mathjs.replace(/e/g, "e");
+
+    // Operators
+    mathjs = mathjs.replace(/\\cdot/g, "*");
+    mathjs = mathjs.replace(/\\times/g, "*");
+    mathjs = mathjs.replace(/\\div/g, "/");
+
+    // Parentheses
+    mathjs = mathjs.replace(/\\left\(/g, "(").replace(/\\right\)/g, ")");
+    mathjs = mathjs.replace(/\\left\[/g, "[").replace(/\\right\]/g, "]");
+
+    // Summation and Product
+    mathjs = mathjs.replace(/\\sum_{([^{}]+)}\^([^{}]+) ([^ ]+)/g, "sum($3)");
+    mathjs = mathjs.replace(/\\prod_{([^{}]+)}\^([^{}]+) ([^ ]+)/g, "prod($3)");
+
+    // Absolute value
+    mathjs = mathjs.replace(/\\left\|([^|]+)\\right\|/g, "abs($1)");
+
+    // Statistical functions
+    mathjs = mathjs.replace(/\\text{mean}\(([^)]+)\)/g, "mean($1)");
+    mathjs = mathjs.replace(/\\text{median}\(([^)]+)\)/g, "median($1)");
+    mathjs = mathjs.replace(/\\text{variance}\(([^)]+)\)/g, "var($1)");
+    mathjs = mathjs.replace(/\\text{stdev}\(([^)]+)\)/g, "std($1)");
+    mathjs = mathjs.replace(/\\text{cov}\(([^,]+), ([^)]+)\)/g, "cov($1, $2)");
+    mathjs = mathjs.replace(/\\text{cor}\(([^,]+), ([^)]+)\)/g, "corr($1, $2)");
+
+    // Percentage
+    // mathjs = mathjs.replace(/\\%/g, "/100");
+
+    // Replace standalone `\%` with `/100`
+    mathjs = mathjs.replace(/([a-zA-Z0-9]+)\\%$/, "($1/100)");
+
+    // Replace `\%` followed by a number or variable (e.g., "5\%100") with divide by that number
+    mathjs = mathjs.replace(/([a-zA-Z0-9]+)\\%([a-zA-Z0-9]+)/g, "($1%$2)");
+
+    // Modulo
+    mathjs = mathjs.replace(
+      /\\text{mod}\{([^{}]+)\}\{([^{}]+)\}/g,
+      "mod($1, $2)"
+    );
+
+    return mathjs;
+  };
+
+  useEffect(() => {
+    if (expression === "") {
+      setResult("");
+    }
+  }, [expression]);
+
+  const arrayToObject = (array) => {
+    return array?.length > 0
+      ? array?.reduce((acc, { label, value }) => {
+          acc[label] = value;
+          return acc;
+        }, {})
+      : {};
+  };
+
+  const handleClick = (input) => {
+    setExpression((prevExpression) => prevExpression + ` ${input} `);
+  };
+
+  const handleChange = (e) => {
+    const rawLatex = e?.latex();
+    const formattedLatex = rawLatex.replace(/\\cdot/g, "\\times");
+
+    setExpression(formattedLatex);
+  };
+
+  const clearScreen = () => {
+    setExpression("");
+    setResult("");
+  };
+
+  const backspace = () => {
+    const newExpression = expression.slice(0, -1);
+    setExpression(newExpression);
+  };
+
+  const calculate = () => {
+    try {
+      const allVariables = {
+        ...arrayToObject(customVariables),
+        ...arrayToObject(definedVariables),
+      };
+
+      if (expression !== "") {
+        const mathExpression = latexToMathjs(expression);
+        const evaluateResult = math.evaluate(mathExpression, allVariables);
+
+        if (typeof evaluateResult === "number" && !isNaN(evaluateResult)) {
+          setResult(evaluateResult.toFixed(3).replace(/\.000$/, ""));
+        } else {
+          setResult("Error: Invalid expression");
+        }
+      }
+    } catch (error) {
+      setResult("Error: Invalid expression");
+    }
+  };
+
+  return {
+    definedVariables,
+    digitKeys,
+    operationsKeys,
+    expression,
+    result,
+    handleClick,
+    handleChange,
+    clearScreen,
+    backspace,
+    calculate,
+    handleKeyPress,
+    mathQuillConfig,
+    removeVariable,
+    customVariables,
+    setCustomVariables,
+  };
+};
